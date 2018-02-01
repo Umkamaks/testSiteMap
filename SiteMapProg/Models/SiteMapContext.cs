72725bb2fae2db3e3729a4130467c84092c69055
Maksim <pogoncev.maksim@mail.ru>
2018-02-01 20:26:57 +0200

    last

--- SiteMapProg/Models/SiteMapContext.cs
+++ SiteMapProg/Models/SiteMapContext.cs
@@ -8,11 +8,11 @@ namespace SiteMapProg.Models
 {
     public class SiteMapContext : DbContext
     {
-        
+
         static SiteMapContext()
         {
-            
-               Database.SetInitializer<SiteMapContext>(new MyContextInitializer());
+
+            Database.SetInitializer<SiteMapContext>(new MyContextInitializer());
         }
         public SiteMapContext(): base("DefaultConnection") {  }
 
@@ -24,14 +24,14 @@ namespace SiteMapProg.Models
         protected override void Seed(SiteMapContext db)
         {
 
-            MapSite mapSite = new MapSite() { NameSite = "Podrobnosti.uaffff" };
+            MapSite mapSite = new MapSite() { NameSite = "http://podrobnosti.ua" };
             db.MapSites.Add(mapSite);
             db.SaveChanges();
 
 
-            SiteMapUrl siteMaps1 = new SiteMapUrl() { MapSite= mapSite, TimeOut = 1, UrlSite = "Podrobnosti" };
-            SiteMapUrl siteMaps2 = new SiteMapUrl() { MapSite = mapSite, TimeOut = 1, UrlSite = "Podrobnosti" };
-            SiteMapUrl siteMaps3 = new SiteMapUrl() {MapSite = mapSite, TimeOut = 1, UrlSite = "Podrobnosti" };
+            SiteMapUrl siteMaps1 = new SiteMapUrl() { MapSite= mapSite, TimeOut = 1, UrlSite = "http://podrobnosti.ua/2223807-v-ankare-progremel-vzryv-pervye-foto-i-video.html" };
+            SiteMapUrl siteMaps2 = new SiteMapUrl() { MapSite = mapSite, TimeOut = 1, UrlSite = "http://podrobnosti.ua/2223806-pod-zhitomirom-zastrelilsja-ukrainskij-voennyj.html" };
+            SiteMapUrl siteMaps3 = new SiteMapUrl() {MapSite = mapSite, TimeOut = 1, UrlSite = "http://podrobnosti.ua/2223767-pogoda-na-2-fevralja-ukrainu-nakrojut-dozhdi.html" };
             db.SiteMapsUrls.AddRange(new List<SiteMapUrl> { siteMaps1, siteMaps2, siteMaps3 });
             db.SaveChanges();
 
